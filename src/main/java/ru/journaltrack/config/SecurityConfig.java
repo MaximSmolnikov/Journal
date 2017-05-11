ee353a362d223615e672fe065b324a3217694533
Maxim <maksimsmolnikov1@gmail.com>
2017-05-11 22:07:22 +0400

    some changes

--- src/main/java/ru/journaltrack/config/SecurityConfig.java
+++ src/main/java/ru/journaltrack/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/webjars/**","/js/*","/css/*","/","/user/create","/neworder","/add/*").permitAll()
+                .antMatchers("/webjars/**","/js/*","/img/**","/css/*","/","/user/create","/neworder","/add/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
